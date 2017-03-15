285aaf63a70c2eeafe3a809aaf0892d742f0fb0d
Matias Cascallares <matiascas@gmail.com>
2017-03-16 06:13:04 +0800

    added ES persistence

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from elasticsearch import Elasticsearch
+from datetime import datetime
 from quiz import quiz
 
 app = Flask(__name__)
 app.secret_key = 'dfuy48yerhfjdbsklueio'
 
-#app.config['ELASTICSEARCH_URL'] = 'http://localhost:9200/'
-#es = Elasticsearch([app.config['ELASTICSEARCH_URL']])
+app.config['ELASTICSEARCH_URL'] = 'http://localhost:9200/'
+es = Elasticsearch([app.config['ELASTICSEARCH_URL']])
 
 @app.route('/')
 def index():
@@ -15,13 +16,19 @@ def index():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    answers = request.form.to_dict()
-    correct = True
-    for q in quiz:
-        if answers[q['name']] != [i for i in q['options'] if i['correct']][0]['answer']:
-            correct = False
+    form = request.form.to_dict()
+    doc = {
+        'correct': True,
+        'timestamp': datetime.now(),
+        'email' : form['email']
+    }
 
-    # TODO index response in ES
+    for q in quiz:
+        doc[q['name']] = form[q['name']]
+        if form[q['name']] != [i for i in q['options'] if i['correct']][0]['answer']:
+            doc['correct'] = False
+
+    res = es.index(index='esquiz', doc_type='answer', body=doc)
     flash('Thank you!') #TODO show this
     return redirect(url_for('index'))
 
