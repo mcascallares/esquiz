0af341de0df06025441acea4dbd80a8828cd8509
Matias Cascallares <matiascas@gmail.com>
2017-03-16 06:01:00 +0800

    added response validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from elasticsearch import Elasticsearch
+from quiz import quiz
 
 app = Flask(__name__)
 app.secret_key = 'dfuy48yerhfjdbsklueio'
@@ -7,15 +8,19 @@ app.secret_key = 'dfuy48yerhfjdbsklueio'
 #app.config['ELASTICSEARCH_URL'] = 'http://localhost:9200/'
 #es = Elasticsearch([app.config['ELASTICSEARCH_URL']])
 
-
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', quiz=quiz)
 
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    print request.form.keys
+    answers = request.form.to_dict()
+    correct = True
+    for q in quiz:
+        if answers[q['name']] != [i for i in q['options'] if i['correct']][0]['answer']:
+            correct = False
+
     # TODO index response in ES
     flash('Thank you!') #TODO show this
     return redirect(url_for('index'))
---
+++ quiz.py
@@ -0,0 +1,20 @@
+quiz = [
+    { 
+        'name' : 'q1',
+        'question' : 'What is the default number of shards in Elasticsearch?',
+        'options' : [
+            {'answer' : '1', 'correct' : False},
+            {'answer' : '3', 'correct' : False},
+            {'answer' : '5', 'correct' : True}
+        ]
+    },
+
+    {
+        'name' : 'q2',
+        'question' : 'Elasticsearch relies in Apache Lucene',
+        'options' : [
+            {'answer' : 'True', 'correct' : True},
+            {'answer' : 'False', 'correct' : False}
+        ]
+    },
+]
